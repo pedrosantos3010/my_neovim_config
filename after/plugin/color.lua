d031816f7cdd0c8697d5cd5bd08a16b17db72ddd
Pedro Santos <pedrosantos3010@gmail.com>
2024-07-14 17:29:31 -0300

    ✨ add: my nvim setup

---
+++ after/plugin/color.lua
@@ -0,0 +1,11 @@
+
+function ColorMyPencils(color)
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, 'ColorColumn', {ctermbg=0, bg='#403c17'})
+end
+
+ColorMyPencils()
